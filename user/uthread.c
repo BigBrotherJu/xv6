e74793bb7bfb4f0631253ace98e96c2a9507464c
Juhan Jin <juhanjin.me@gmail.com>
2022-05-10 20:24:21 +0800

    uthread done

--- user/uthread.c
+++ user/uthread.c
@@ -11,13 +11,33 @@
 #define MAX_THREAD  4
 
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context saved_regs;     // saved registers
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context* old, struct context* new);
               
 void 
 thread_init(void)
@@ -58,10 +78,8 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    // Invoke thread_switch to switch from t to next_thread:
+    thread_switch(&t->saved_regs, &next_thread->saved_regs);
   } else
     next_thread = 0;
 }
@@ -75,7 +93,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->saved_regs.ra = (uint64)func;
+  t->saved_regs.sp = (uint64)&t->stack + (STACK_SIZE - 1);
 }
 
 void 
